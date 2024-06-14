5d08921d6e564befe2c422d2db7685228ef110d0
Your Name <you@example.com>
2024-06-14 16:04:04 -0700

    updating final project

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 
@@ -13,6 +14,20 @@ public class ProjectService {
 	public List<Project> fetchAllProjects() {
 
 		return projectDao.fetchAllProjects();
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if (!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+
+	}
+
+	public void deleteProject(Integer projectId) {
+		if (!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+
 	}
 
 	public Project fetchProjectById(Integer projectId) {
@@ -26,4 +41,5 @@ public class ProjectService {
 		return projectDao.insertProject(project);
 
 	}
+
 }
